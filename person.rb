a81e65f5bceea44fb08853a34fc5b97cde5d63b7
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-08 15:26:27 -0600

    Create runtime.rb file

--- person.rb
+++ person.rb
@@ -1,8 +1,5 @@
 require 'securerandom'
 require_relative './nameable'
-require_relative './cap_decorator'
-require_relative './base_decorator'
-require_relative './trim_decorator'
 
 class Person < Nameable
   # Define the constructor method with three parameters:
@@ -46,14 +43,3 @@ class Person < Nameable
     @id >= 18
   end
 end
-
-# Creates a new instance of the Person object with 22 as age and 'maximilianus' as name
-person = Person.new(22, 'maximilianus')
-# prints out into the console the string 'maximilianus'
-puts person.correct_name
-capitalized_person = CapDecorator.new(person)
-# prints out into the console the string 'Maximilianus'
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
-# prints out into the console the string 'Maximilian'
-puts capitalized_trimmed_person.correct_name
