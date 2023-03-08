2d49d2bf2b537bfaacf78d7ba58e37119e6715d5
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-08 17:18:25 -0600

    Delete comments

--- person.rb
+++ person.rb
@@ -2,27 +2,20 @@ require 'securerandom'
 require_relative './nameable'
 
 class Person < Nameable
-  # Define the constructor method with three parameters:
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    # Generate a random UUID for each new Person object:
     @id = SecureRandom.uuid
-    # Set the name, age & parent_permission of the Person:
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  # Define attribute accessor methods for name and age:
   attr_accessor :name
   attr_accessor :age
 
-  # Define an attribute reader method for the ID:
   attr_reader :id
 
-  # Define a method to determine if the person is allowed to use certain services:
   def can_use_services?
-    # If the person is of age or has parental permission, they can use the services:
     if of_age? | @parent_permission
       true
     else
@@ -30,16 +23,13 @@ class Person < Nameable
     end
   end
 
-  # Define a method that overrides the correct_name from Nameable parent so that it return the @name attribute
   def correct_name
     @name
   end
 
   private
 
-  # Define a private method to determine if the person is of age:
   def of_age?
-    # If the person's ID indicates they are 18 or older, they are of age:
     @id >= 18
   end
 end
--- runtine.rb
+++ runtine.rb
@@ -2,13 +2,9 @@ require_relative './person'
 require_relative './cap_decorator'
 require_relative './trim_decorator'
 
-# Creates a new instance of the Person object with 22 as age and 'maximilianus' as name
 person = Person.new(22, 'maximilianus')
-# prints out into the console the string 'maximilianus'
 puts person.correct_name
 capitalized_person = CapDecorator.new(person)
-# prints out into the console the string 'Maximilianus'
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
-# prints out into the console the string 'Maximilian'
 puts capitalized_trimmed_person.correct_name
