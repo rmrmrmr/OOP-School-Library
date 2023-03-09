3b7898744da2a3a2e00e2efe5215aaa695c386f8
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-09 16:30:35 -0600

    Require date on rental & add command line example prompts

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
-['person.rb', 'cap_decorator.rb', 'trim_decorator.rb', 'student.rb', 'classroom.rb'].each { |x| require_relative x }
+['person.rb', 'cap_decorator.rb', 'trim_decorator.rb', 'student.rb', 'classroom.rb', 'rental.rb', 'book.rb'].each do |x|
+  require_relative x
+end
 
 # person = Person.new(22, 'maximilianus')
 # puts person.correct_name
@@ -7,19 +9,35 @@
 # capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
 # puts capitalized_trimmed_person.correct_name
 
-alex = Student.new(22, 'One', 'Alex', parent_permission: false)
-one = Classroom.new('One')
-alex.classroom
-one.add_student(alex)
-alex.classroom
-alex.classroom.label
-one.students
-puts one.students_names
+# alex = Student.new(22, 'One', 'Alex', parent_permission: false)
+# one = Classroom.new('One')
+# alex.classroom
+# one.add_student(alex)
+# alex.classroom
+# alex.classroom.label
+# one.students
+# puts one.students_names
 
-john = Student.new(24, 'One', 'John', parent_permission: true)
-john.classroom
-one.add_student(john)
-john.classroom
-john.classroom.label
-one.students
-puts one.students_names
+# john = Student.new(24, 'One', 'John', parent_permission: true)
+# john.classroom
+# one.add_student(john)
+# john.classroom
+# john.classroom.label
+# one.students
+# puts one.students_names
+
+# rent_one = Rental.new(Date.new(2022, 01, 22), 'Ethics', 'Bob')
+# bob = Person.new(22, 'Bob')
+# puts rent_one.person
+# bob.add_rental(rent_one)
+# puts rent_one.person
+# puts rent_one.person.name
+# puts bob.rentals
+
+# ethics = Book.new('Ethics', 'Spinoza')
+# puts rent_one.book
+# ethics.add_rental(rent_one)
+# puts rent_one.book
+# puts rent_one.book.title
+# puts rent_one.book.author
+# puts ethics.rentals
