37f66d434d8c2e72ab2c79fda04b76bf5d5af16e
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-09 15:59:42 -0600

    Establish book has-many rental relationship

--- rental.rb
+++ rental.rb
@@ -1,7 +1,14 @@
 class Rental
   attr_accessor :date
+  attr_reader :book
   
-  def initialize(date)
+  def initialize(date, book)
     @date = date
+    @book = book
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
   end
 end
